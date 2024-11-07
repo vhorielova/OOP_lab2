c222b7a0bb10f0b23d30d930269682dcb7fd67f8
vhorielova <horielova.viktoria@gmail.com>
2024-11-07 13:51:22 +0200

    print data through SAX parser

--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,17 +1,39 @@
 package org.main;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.tariff.Tariff;
+import org.tariff.Tariff.Plan;
+import org.parsers.TariffSAXParser;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("SAX parser");
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            TariffSAXParser handler = new TariffSAXParser();
+            saxParser.parse(new File("src/main/resources/tariff.xml"), handler);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Tariff tariff = handler.getTariff();
+            // Output result to verify
+            for (Plan plan : tariff.getPlan()) {
+                System.out.println("Plan Name: " + plan.getName());
+                System.out.println("Operator: " + plan.getOperatorName());
+                System.out.println("Payroll: " + plan.getPayroll());
+                System.out.println("Inside Network Call Price: " + plan.getCallPrices().getInsideNetwork());
+                System.out.println("Outside Network Call Price: " + plan.getCallPrices().getOutsideNetwork());
+                System.out.println("Landline Call Price: " + plan.getCallPrices().getLandline());
+                System.out.println("SMS Price: " + plan.getSmsPrice());
+                System.out.println("Favorite Number: " + plan.getParameters().getFavoriteNumber());
+                System.out.println("Billing: " + plan.getParameters().getBilling());
+                System.out.println("Connection Fee: " + plan.getParameters().getConnectionFee());
+                System.out.println("---------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
